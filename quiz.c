e69ed7917a0616cffc37c9f3e72b44439ec4c20c
monachit <moadnachit6@gmail.com>
2023-04-05 05:09:04 -0700

    noice

--- quiz.c
+++ quiz.c
@@ -8,7 +8,7 @@ void quiz()
     printf("Welcome to the quiz game!\n");
     printf("Question 1 :In which category belongs the C programming language? \n");
     printf("A) Compiled language \nB) Interpreted language\n");
-    scanf("%c", &n);
+    scanf(" %c", &n);
     if (n == 'A')
     {
         printf("Correct!\n");
@@ -21,7 +21,7 @@ void quiz()
     
     printf("Question 2: What is the largest planet in our solar system?\n");
     printf("A) .hpp\nB) .ch\nC) .h\n");
-    scanf("%c", &n);
+    scanf(" %c", &n);
     if (n == 'C')
     {
         printf("Correct!\n");
@@ -33,7 +33,7 @@ void quiz()
     }
     printf("Question 3: We declare the following variable int arr[5];?\n");
     printf("A) arr + 2\nB) *arr + 2\nC) *(arr + 2)\n");
-    scanf("%c", &n);
+    scanf(" %c", &n);
     if (n == 'C')
     {
         printf("Correct!\n");
@@ -43,9 +43,9 @@ void quiz()
     {
         printf("Wrong!\n");
     }
-    printf("Question 5: Is it possible to declare a pointer to a pointer?\n");
+    printf("Question 4: Is it possible to declare a pointer to a pointer?\n");
     printf("A) yes\nB) no\nC) It depends on the type the pointer is pointing to\n");
-    scanf("%c", &n);
+    scanf(" %c", &n);
     if (n == 'A')
     {
         printf("Correct!\n");
@@ -55,9 +55,9 @@ void quiz()
     {
         printf("Wrong!\n");
     }
-      printf("Question 6: What is the size of a pointer to an int (on a 64-bit architecture)?\n");
+      printf("Question 5: What is the size of a pointer to an int (on a 64-bit architecture)?\n");
       printf("A) 8 bytes\nB) 4 bytes\nC) 1 bytes\n");
-      scanf("%c", &n);
+      scanf(" %c", &n);
       if (n == 'A')
       {
         printf("Correct!\n");
@@ -67,7 +67,7 @@ void quiz()
     {
         printf("Wrong!\n");
     }
-    printf("Your score is: %d out of 6\n", score);
+    printf("Your score is: %d out of 5\n", score);
 }
 int main(void)
 {
